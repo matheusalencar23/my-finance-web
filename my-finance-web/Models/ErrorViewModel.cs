9e3b77b9ebb78dd581cd7768a6da3452916ee43b
Matheus Alencar <matheusalencar6942@gmail.com>
2024-03-27 20:54:41 -0300

    Commit inicial

---
+++ my-finance-web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace my_finance_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
