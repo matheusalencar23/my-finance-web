0f90f773d0f203defca89ec90d14c06462a0d21c
Matheus Alencar <matheusalencar6942@gmail.com>
2024-05-27 07:30:18 -0300

    Extraindo string de conexão com o banco para o appsettings de desenvolvimento

--- my-finance-web-infra/MyFinanceDbContext.cs
+++ my-finance-web-infra/MyFinanceDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using my_finance_web_domain.Entities;
 
 namespace my_finance_web_infra;
 public class MyFinanceDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<PlanoConta> PlanoConta { get; set; }
     public DbSet<Transacao> Transacao { get; set; }
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=myfinance;User Id=SA;Password=1q2w3e4r@#$;TrustServerCertificate=true;");
+        var connectionString = _configuration.GetConnectionString("Database");
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
